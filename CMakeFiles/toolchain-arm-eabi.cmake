19dc78044f31fa8c3962f1561b04e7d563b9cf1c
Erik Bakker <e.e.bakker@online.nl>
2025-10-05 11:14:41 +0200

    CMakeList.txt changes

--- CMakeFiles/toolchain-arm-eabi.cmake
+++ CMakeFiles/toolchain-arm-eabi.cmake
@@ -6,8 +6,8 @@ set(TOOLCHAIN_PREFIX "C:/Program Files (x86)/Arm GNU Toolchain arm-none-eabi/14.
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_ASM_COMPILER arm-none-eabi-as)
 
-set(CMAKE_C_FLAGS "-Wall -O2 -nostdlib -nostartfiles -ffreestanding" CACHE STRING "" FORCE)
-set(CMAKE_C_FLAGS_DEBUG "-Wall -O2 -nostdlib -nostartfiles -ffreestanding -g3 -Og" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS "-Wall -nostdlib -nostartfiles -ffreestanding" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS_DEBUG "-Wall -nostdlib -nostartfiles -ffreestanding -g3 -Og" CACHE STRING "" FORCE)
 
 set(CMAKE_ASM_FLAGS "--warn --fatal-warnings" CACHE STRING "" FORCE)
 
